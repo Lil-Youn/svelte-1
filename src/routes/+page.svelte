618f48e16b5273198cf67d81685878bd0b617eab
Lil-Youn <110492763+Lil-Youn@users.noreply.github.com>
2023-08-20 20:54:55 +0200

    add TopNavBar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,28 @@
 <script>
+  import TopNavbar from "$lib/top-navbar/+page.svelte";
   let src = "/src/assets/whale.png";
   let name = "Simon";
   let buttonText = "Go to Site 1";
+  import Button, { Label } from "@smui/button";
 </script>
 
+<link rel="stylesheet" href="node_modules/svelte-material-ui/bare.css" />
+<TopNavbar />
 <div class="welcome-box">
-  <h1>Hi {name}!Welcome to my first Svelte App</h1>
+  <h1>Hi {name}! Welcome to my first Svelte App</h1>
 </div>
 <img {src} alt="whale" />
-<button class="button">
-  <a href="/site1" target="_blank" rel="noopener noreferrer"> {buttonText} </a>
-</button>
+
+<div class="button-container">
+  <Button
+    variant="outlined"
+    class="button-shaped-round"
+    href="/site1"
+    target="_blank"
+  >
+    <Label>{buttonText}</Label>
+  </Button>
+</div>
 
 <style>
   h1 {
@@ -32,14 +44,8 @@
     height: 25%;
     margin-bottom: 5px;
   }
-  .button {
-    display: block;
-    margin: 0 auto;
-    background-color: lightblue;
-    color: white;
-    padding: 10px 20px;
-    border: none;
-    border-radius: 5px;
-    cursor: pointer;
+  .button-container {
+    display: flex;
+    justify-content: center;
   }
 </style>
