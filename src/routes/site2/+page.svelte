d899dba85a219cfec64e541992375db33848b7ee
Lil-Youn <110492763+Lil-Youn@users.noreply.github.com>
2023-08-20 21:11:03 +0200

    import and add Navbar

---
+++ src/routes/site2/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+  import TopNavbar from "$lib/top-navbar/+page.svelte";
+</script>
+
+<link rel="stylesheet" href="node_modules/svelte-material-ui/bare.css" />
+<TopNavbar />
+
+<h1>Site 2</h1>
+<h2>Site 2</h2>
+<h3>Site 2</h3>
+<h4>Site 2</h4>
+<h5>Site 2</h5>
+<h6>Site 2</h6>
