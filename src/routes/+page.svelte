45433ffdb681ecac7f3e388c8afab94635bfcde6
Lil-Youn <110492763+Lil-Youn@users.noreply.github.com>
2023-08-20 21:10:40 +0200

    remove unused Button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,6 @@
   import TopNavbar from "$lib/top-navbar/+page.svelte";
   let src = "/src/assets/whale.png";
   let name = "Simon";
-  let buttonText = "Go to Site 1";
-  import Button, { Label } from "@smui/button";
 </script>
 
 <link rel="stylesheet" href="node_modules/svelte-material-ui/bare.css" />
@@ -13,17 +11,6 @@
 </div>
 <img {src} alt="whale" />
 
-<div class="button-container">
-  <Button
-    variant="outlined"
-    class="button-shaped-round"
-    href="/site1"
-    target="_blank"
-  >
-    <Label>{buttonText}</Label>
-  </Button>
-</div>
-
 <style>
   h1 {
     color: darkgrey;
@@ -31,7 +18,7 @@
     text-decoration: underline;
   }
   .welcome-box {
-    margin: 0 auto;
+    margin: 5px auto;
     width: 50%;
     border: 1px solid black;
     border-radius: 25px;
@@ -43,9 +30,5 @@
     width: 25%;
     height: 25%;
     margin-bottom: 5px;
-  }
-  .button-container {
-    display: flex;
-    justify-content: center;
   }
 </style>
