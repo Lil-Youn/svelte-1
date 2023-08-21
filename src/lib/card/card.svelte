6bbbd00e6e4fbcb97ce903c34c76dcd29cc26e32
Lil-Youn <110492763+Lil-Youn@users.noreply.github.com>
2023-08-21 20:43:59 +0200

    add card component

---
+++ src/lib/card/card.svelte
@@ -0,0 +1,27 @@
+<div class="w-1/2 flex justify-center mb-5">
+  <div
+    class="h-48 lg:h-auto lg:w-48 flex-none bg-contain rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden"
+    style="background-image: url('/src/assets/whale.png')"
+    title="whale"
+  />
+  <div
+    class="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal"
+  >
+    <div class="mb-8">
+      <div class="text-gray-900 font-bold text-xl mb-2">
+        Hi, its me Simon! First try on Svelte.... :)
+      </div>
+      <p class="text-gray-700 text-base">
+        I could write a lot of stuff here, but I am not going to do that. I am
+        just going to say that I am ChefDev and I am going to make a lot of cool
+        stuff with Svelte.
+      </p>
+    </div>
+    <div class="flex items-center">
+      <div class="text-sm">
+        <p class="text-gray-900 leading-none">Simon Flader</p>
+        <p class="text-gray-600">ChefDev</p>
+      </div>
+    </div>
+  </div>
+</div>
