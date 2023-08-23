2a27447f7c52a261faa2229be9f2a6dff0de2e4d
Lil-Youn <110492763+Lil-Youn@users.noreply.github.com>
2023-08-23 20:08:22 +0200

    add new card component

--- src/lib/card/card.svelte
+++ src/lib/card/card.svelte
@@ -1,29 +1,20 @@
-<div
-  class="w-1/2 flex justify-center mb-5 border-b border-t border-l border-r border-gray-800 rounded-b rounded-t"
->
-  <div
-    class="h-48 lg:h-auto lg:w-48 flex-none bg-contain rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden"
-    style="background-image: url('/src/assets/whale.png')"
-    title="whale"
-  />
-  <div
-    class="  bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal"
-  >
-    <div class="mb-8">
-      <div class="text-gray-900 font-bold text-xl mb-2">
-        Hi, its me Simon! First try on Svelte.... :)
-      </div>
-      <p class="text-gray-700 text-base">
-        I could write a lot of stuff here, but I am not going to do that. I am
-        just going to say that I am ChefDev and I am going to make a lot of cool
-        stuff with Svelte.
-      </p>
-    </div>
-    <div class="flex items-center">
-      <div class="text-sm">
-        <p class="text-gray-900 leading-none">Simon Flader</p>
-        <p class="text-gray-600">ChefDev</p>
-      </div>
-    </div>
-  </div>
+<script>
+  import { Card, Button, Toggle } from "flowbite-svelte";
+  import { Icon } from "flowbite-svelte-icons";
+  let vCard = false;
+</script>
+
+<div style="width: 300px; height: 300px; margin-bottom: 10vh;">
+  <Card img="/src/assets/whale.png" reverse={vCard} class="mb-2">
+    <h5
+      class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
+    >
+      Hi, its me Simon! First try on Svelte.... :)
+    </h5>
+    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 leading-tight">
+      I could write a lot of stuff here, but I am not going to do that. I am
+      just going to say that I am ChefDev and I am going to make a lot of cool
+      stuff with Svelte.
+    </p>
+  </Card>
 </div>
