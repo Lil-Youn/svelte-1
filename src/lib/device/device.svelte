ab47cd2ddb218d7d1ba200a2d11350748ba0bc5f
Lil-Youn <110492763+Lil-Youn@users.noreply.github.com>
2023-08-23 21:32:40 +0200

    add device component

---
+++ src/lib/device/device.svelte
@@ -0,0 +1,24 @@
+<script>
+  import { DeviceMockup } from "flowbite-svelte";
+</script>
+
+<div class="small-device">
+  <DeviceMockup device="ios">
+    <img
+      src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/hero/mockup-2-light.png"
+      class="dark:hidden w-[272px] h-[572px]"
+      alt="ios example 1"
+    />
+    <img
+      src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/hero/mockup-2-dark.png"
+      class="hidden dark:block w-[272px] h-[572px]"
+      alt="ios example 2"
+    />
+  </DeviceMockup>
+</div>
+
+<style>
+  .small-device {
+    transform: scale(0.75);
+  }
+</style>
