10d27b2d328eed0a5181cb215ce121cf8e19dd57
Lil-Youn <110492763+Lil-Youn@users.noreply.github.com>
2023-08-23 21:33:31 +0200

    build site 3

---
+++ src/routes/site3/+page.svelte
@@ -0,0 +1,15 @@
+<header>
+  <div class="flex justify-center mt-10 mb-20">
+    <div class="shadow-md rounded-md w-1/2">
+      <p class="font-mono text-lg text-gray-800 text-center">Site 3</p>
+    </div>
+  </div>
+</header>
+<main>
+  <h1>Site 3</h1>
+  <h2>Site 3</h2>
+  <h3>Site 3</h3>
+  <h4>Site 3</h4>
+  <h5>Site 3</h5>
+  <h6>Site 3</h6>
+</main>
