076bbbc5129de4d9531a744c1a90e1f7cbedbace
Lil-Youn <110492763+Lil-Youn@users.noreply.github.com>
2023-08-23 21:33:20 +0200

    add <Device> to site2

--- src/routes/site2/+page.svelte
+++ src/routes/site2/+page.svelte
@@ -1,15 +1,14 @@
+<script>
+  import Device from "$lib/device/device.svelte";
+</script>
+
 <header>
-  <div class="flex justify-center mt-10 mb-20">
+  <div class="flex justify-center mt-10 mb-10">
     <div class="shadow-md rounded-md w-1/2">
       <p class="font-mono text-lg text-gray-800 text-center">Site 2</p>
     </div>
   </div>
 </header>
 <main>
-  <h1>Site 2</h1>
-  <h2>Site 2</h2>
-  <h3>Site 2</h3>
-  <h4>Site 2</h4>
-  <h5>Site 2</h5>
-  <h6>Site 2</h6>
+  <Device />
 </main>
