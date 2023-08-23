04751e1e024913b77214a83be2991791f0042d7f
Lil-Youn <110492763+Lil-Youn@users.noreply.github.com>
2023-08-23 20:08:35 +0200

    add darkmode switch

---
+++ src/lib/darkmode/darkmode.svelte
@@ -0,0 +1,13 @@
+<script>
+  import { DarkMode } from "flowbite-svelte";
+  import { Icon } from "flowbite-svelte-icons";
+</script>
+
+<DarkMode class="text-lg">
+  <svelte:fragment slot="lightIcon">
+    <Icon name="sun-solid" />
+  </svelte:fragment>
+  <svelte:fragment slot="darkIcon">
+    <Icon name="moon-solid" />
+  </svelte:fragment>
+</DarkMode>
