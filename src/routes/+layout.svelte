31d9bdf779a989fdcd82a022d5617c9c6d137b9e
Lil-Youn <110492763+Lil-Youn@users.noreply.github.com>
2023-08-25 15:01:18 +0200

    import banner/ add sections (header,main) to layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,21 @@
 <script lang="ts">
   import "../app.postcss";
-  import TopNavbar from "$lib/top-navbar/+page.svelte";
+  import TopNavbar from "$lib/top-navbar/bottom-nav.svelte";
+  import Banner from "$lib/banner/banner.svelte";
 </script>
 
 <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
-
-<slot />
+<header>
+  <Banner />
+</header>
+<main>
+  <slot />
+</main>
 <TopNavbar />
+
+<style>
+  main {
+    padding: 1rem;
+    margin-top: 2rem;
+  }
+</style>
